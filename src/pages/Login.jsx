08bcd4c88f3246a174b7ea6d82d0437a5d27dd21
Dipesh Shivrame <dipeshss@Dipeshs-MacBook-Air.local>
2023-08-09 01:31:58 +0530

    added styling on register and login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,69 +27,73 @@ function Login() {
 	}
 
 	function loginAsGuest() {
-		dispatch(handleLogin({ username: "LParker22", password: "LParker22"}))
+		dispatch(handleLogin({ username: "LParker22", password: "LParker22" }))
 	}
 
 	return (
-		<section className="min-h-screen mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8 flex-col md:flex-row  flex justify-center items-center gap-8">
-			<div className="h-32 w-32 md:h-auto md:w-auto">
-				<img src={loginImage} alt="Login Logo Image" />
+		<section className="min-h-screen mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8 flex-col flex justify-center items-center gap-8">
+			<div className="text-center">
+				<h1 className="text-3xl font-bold text-[#1DA1F2]">VibeVerse</h1>
 			</div>
-			<div className="max-w-sm w-full">
-				<h2 className="font-semibold text-xl mb-4">Login To Your Account</h2>
-				<form
-					className="w-full bg-white rounded py-4"
-					onSubmit={handleSubmit}
-				>
-					<div className="mb-4">
-						<label className="block text-gray-700 text-sm font-medium mb-2" htmlFor="username">
-							Username
-						</label>
-						<input
-							className="text-xs border border-gray-300 focus:border-blue-300 outline-none py-2 px-3 rounded w-full focus:transition-colors duration-1000"
-							id="username"
-							type="text"
-							placeholder="Enter your username"
-							value={username}
-							onChange={(e) => setUsername(e.target.value)}
-						/>
+			<div className="flex flex-col md:flex-row justify-center items-center gap-8">
+				<div className="h-32 w-32 md:h-auto md:w-auto">
+					<img src={loginImage} alt="Login Logo Image" />
+				</div>
+				<div className="max-w-sm w-full">
+					<h2 className="font-semibold text-lg mb-4">Login To Your Account</h2>
+					<form
+						className="w-full bg-white rounded pb-4"
+						onSubmit={handleSubmit}
+					>
+						<div className="mb-4">
+							<label className="block text-gray-700 text-sm font-medium mb-2" htmlFor="username">
+								Username
+							</label>
+							<input
+								className="text-xs border border-gray-300 focus:border-blue-300 outline-none py-2 px-3 rounded w-full focus:transition-colors duration-1000"
+								id="username"
+								type="text"
+								placeholder="Enter your username"
+								value={username}
+								onChange={(e) => setUsername(e.target.value)}
+							/>
+						</div>
+						<div className="mb-4">
+							<label className="block text-gray-700 text-sm font-medium mb-2" htmlFor="password">
+								Password
+							</label>
+							<input
+								className="text-xs border border-gray-300 focus:border-blue-300 outline-none py-2 px-3 rounded w-full focus:transition-colors duration-1000"
+								id="password"
+								type="password"
+								placeholder="Enter your password"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+							/>
+						</div>
+						<div className="flex items-center justify-between mb-2">
+							<button
+								className="w-full bg-[#1DA1F2] text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+								type="submit"
+							>
+								Log In
+							</button>
+						</div>
+						<div className="flex items-center justify-between">
+							<button onClick={loginAsGuest} type="button"
+								className="w-full bg-gray-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+								Log in as a guest
+							</button>
+						</div>
+					</form>
+					<div>
+						<span className="text-sm">Don't have an account yet?</span>
+						<NavLink to="/signup" className={`flex items-center`} >
+							<span className="text-sm underline text-blue-400">Join us to vibe!!</span>
+						</NavLink>
 					</div>
-					<div className="mb-4">
-						<label className="block text-gray-700 text-sm font-medium mb-2" htmlFor="password">
-							Password
-						</label>
-						<input
-							className="text-xs border border-gray-300 focus:border-blue-300 outline-none py-2 px-3 rounded w-full focus:transition-colors duration-1000"
-							id="password"
-							type="password"
-							placeholder="Enter your password"
-							value={password}
-							onChange={(e) => setPassword(e.target.value)}
-						/>
-					</div>
-					<div className="flex items-center justify-between mb-2">
-						<button
-							className="w-full bg-blue-500 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-							type="submit"
-						>
-							Log In
-						</button>
-					</div>
-					<div className="flex items-center justify-between">
-						<button onClick={loginAsGuest} type="button"
-							className="w-full bg-gray-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-							Log as a guest
-						</button>
-					</div>
-				</form>
-				<div>
-					<span className="text-sm">Don't have an account yet?</span>
-					<NavLink to="/signup" className={`flex items-center`} >
-						<span className="text-sm underline text-blue-400">Join us to vibe!!</span>
-					</NavLink>
 				</div>
 			</div>
-
 		</section>
 	);
 };
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -37,104 +37,109 @@ function Signup() {
 	};
 
 	return (
-		<section className="min-h-screen mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8 flex-col md:flex-row flex justify-center items-center gap-8">
-			<div className="h-32 w-32 md:h-auto md:w-auto">
-				<img src={RegisterImg} alt="Register Logo Image" />
+		<section className="min-h-screen mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8 flex flex-col justify-center items-center gap-8">
+			<div className="text-center">
+				<h1 className="text-3xl font-bold text-[#1DA1F2]">VibeVerse</h1>
+				<span>Connecting Vibes, One Account at a Time: Join Vibeverse Today!</span>
 			</div>
-			<div className="max-w-sm w-full">
-				<h2 className="font-semibold text-xl">Create An Account</h2>
-				<form
-					className="rounded py-4"
-					onSubmit={(e) => {
-						handleSubmit(e);
-					}}
-				>
-					<div className="mb-4">
-						<label
-							htmlFor="firstName"
-							className="block text-gray-700 text-sm font-medium mb-1"
-						>
-							First Name
-						</label>
-						<input
-							type="text"
-							id="firstName"
-							name="firstName"
-							value={formData.firstName}
-							onChange={handleChange}
-							className="text-xs border border-gray-300 focus:border-blue-300 outline-none py-2 px-3 rounded w-full focus:transition-colors duration-1000"
-							placeholder="Enter your first name"
-							required
-						/>
+			<div className="flex flex-col md:flex-row justify-center items-center gap-8">
+				<div className="h-32 w-32 md:h-auto md:w-auto">
+					<img src={RegisterImg} alt="Register Logo Image" />
+				</div>
+				<div className="max-w-sm w-full">
+					<form
+						className="rounded pb-4"
+						onSubmit={(e) => {
+							handleSubmit(e);
+						}}
+					>
+						<div className="mb-4">
+							<label
+								htmlFor="firstName"
+								className="block text-gray-700 text-sm font-medium mb-1"
+							>
+								First Name
+							</label>
+							<input
+								type="text"
+								id="firstName"
+								name="firstName"
+								value={formData.firstName}
+								onChange={handleChange}
+								className="text-xs border border-gray-300 focus:border-blue-300 outline-none py-2 px-3 rounded w-full focus:transition-colors duration-1000"
+								placeholder="Enter your first name"
+								required
+							/>
+						</div>
+						<div className="mb-4">
+							<label
+								htmlFor="lastName"
+								className="block text-gray-700 text-sm font-medium mb-1"
+							>
+								Last Name
+							</label>
+							<input
+								type="text"
+								id="lastName"
+								name="lastName"
+								value={formData.lastName}
+								onChange={handleChange}
+								className="text-xs border border-gray-300 focus:border-blue-300 outline-none py-2 px-3 rounded w-full focus:transition-colors duration-1000"
+								placeholder="Enter your last name"
+								required
+							/>
+						</div>
+						<div className="mb-4">
+							<label
+								htmlFor="username"
+								className="block text-gray-700 text-sm font-medium mb-1"
+							>
+								Username
+							</label>
+							<input
+								type="text"
+								id="username"
+								name="username"
+								value={formData.username}
+								onChange={handleChange}
+								className="text-xs border border-gray-300 focus:border-blue-300 outline-none py-2 px-3 rounded w-full focus:transition-colors duration-1000"
+								placeholder="Choose a username"
+								required
+							/>
+						</div>
+						<div className="mb-6">
+							<label
+								htmlFor="password"
+								className="block text-gray-700 text-sm font-medium mb-1"
+							>
+								Password
+							</label>
+							<input
+								type="password"
+								id="password"
+								name="password"
+								value={formData.password}
+								onChange={handleChange}
+								className="text-xs border border-gray-300 focus:border-blue-300 outline-none py-2 px-3 rounded w-full focus:transition-colors duration-1000"
+								placeholder="Enter a password"
+								required
+							/>
+						</div>
+						<div className="flex items-center justify-center">
+							<button
+								type="submit"
+								className="button-primary bg-[#1DA1F2] w-full p-2 rounded text-white text-sm"
+							>
+								Sign Up
+							</button>
+						</div>
+					</form>
+					<div>
+						<span className="text-sm">Already have an account?</span>
+						<NavLink to="/login" className={`flex items-center`} >
+							<span className="text-sm underline text-blue-400">Login to vibe!!</span>
+						</NavLink>
 					</div>
-					<div className="mb-4">
-						<label
-							htmlFor="lastName"
-							className="block text-gray-700 text-sm font-medium mb-1"
-						>
-							Last Name
-						</label>
-						<input
-							type="text"
-							id="lastName"
-							name="lastName"
-							value={formData.lastName}
-							onChange={handleChange}
-							className="text-xs border border-gray-300 focus:border-blue-300 outline-none py-2 px-3 rounded w-full focus:transition-colors duration-1000"
-							placeholder="Enter your last name"
-							required
-						/>
-					</div>
-					<div className="mb-4">
-						<label
-							htmlFor="username"
-							className="block text-gray-700 text-sm font-medium mb-1"
-						>
-							Username
-						</label>
-						<input
-							type="text"
-							id="username"
-							name="username"
-							value={formData.username}
-							onChange={handleChange}
-							className="text-xs border border-gray-300 focus:border-blue-300 outline-none py-2 px-3 rounded w-full focus:transition-colors duration-1000"
-							placeholder="Choose a username"
-							required
-						/>
-					</div>
-					<div className="mb-6">
-						<label
-							htmlFor="password"
-							className="block text-gray-700 text-sm font-medium mb-1"
-						>
-							Password
-						</label>
-						<input
-							type="password"
-							id="password"
-							name="password"
-							value={formData.password}
-							onChange={handleChange}
-							className="text-xs border border-gray-300 focus:border-blue-300 outline-none py-2 px-3 rounded w-full focus:transition-colors duration-1000"
-							placeholder="Enter a password"
-							required
-						/>
-					</div>
-					<div className="flex items-center justify-center">
-						<button
-							type="submit"
-							className="button-primary bg-blue-600 w-full p-2 rounded text-white text-sm"
-						>
-							Sign Up
-						</button>
-					</div>
-				</form>
-				<div>
-					<span className="text-sm">Already have an account?</span>
-					<NavLink to="/login" className={`flex items-center`} >
-						<span className="text-sm underline text-blue-400">Login to vibe!!</span>
-					</NavLink>
 				</div>
 			</div>
 		</section>
